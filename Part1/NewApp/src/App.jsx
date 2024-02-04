0584e44f346b21a1fa1440586d9aef414a7300b5
adirajus <saiadi4002@gmail.com>
2024-02-04 20:21:42 +0000

    Exercise 1.9 App.jsx modified

--- Part1/NewApp/src/App.jsx
+++ Part1/NewApp/src/App.jsx
@@ -1,17 +1,27 @@
 import { useState } from 'react'
 
 const Statistics = ({good, bad, neutral}) => {
-  return (
-    <div>
-    <h2>Statistics:</h2>
-    <p>Good {good}</p>
-    <p>Neutral {neutral}</p>
-    <p>Bad {bad}</p>
-    <p> All {good + bad + neutral}</p>
-    <p>Average { (good - bad) / (good + neutral + bad)}</p>
-    <p>Positive = {good / (good + neutral + bad) * 100} %</p>
-  </div>
-  )
+  switch (good + bad + neutral) {
+    case 0:
+      return (
+        <div>
+          <h2>Statistics</h2>
+          <p>No feedback given</p>
+        </div>
+      )
+    default:
+      return (
+        <div>
+        <h2>Statistics:</h2>
+        <p>Good {good}</p>
+        <p>Neutral {neutral}</p>
+        <p>Bad {bad}</p>
+        <p> All {good + bad + neutral}</p>
+        <p>Average { (good - bad) / (good + neutral + bad)}</p>
+        <p>Positive = {good / (good + neutral + bad) * 100} %</p>
+      </div>
+      )
+  }
 }
 
 const App = () => {
