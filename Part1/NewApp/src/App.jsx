eb72bba71c9e68d6d66fdf814d5f270aea9a5b3d
adirajus <saiadi4002@gmail.com>
2024-02-04 11:30:42 +0000

    Exercises 1.5 changes made to App.jsx

--- Part1/NewApp/src/App.jsx
+++ Part1/NewApp/src/App.jsx
@@ -33,25 +33,28 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [{
-    name: 'Fundamentals of React',
-    exercises:  10 
-  },
-  {
-    name: 'Using props to pass data',
-    exercises: 7
-  },
-  {
-    name: "State of a component",
-    exercises: 14
-  }]
-
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
-      <Header course = {course}/>
-      <Part parts = {parts} />
-      <Total parts = {parts} />
+      <Header course = {course.name}/>
+      <Part parts = {course.parts} />
+      <Total parts = {course.parts} />
     </div>
   )
 }
