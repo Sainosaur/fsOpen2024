4c86fc7e0a0000e72b51979b814be12b4cfb2352
adirajus <saiadi4002@gmail.com>
2024-02-04 20:14:15 +0000

    Exercises 1.6 and 1.7 updates App.jsx

--- Part1/NewApp/src/App.jsx
+++ Part1/NewApp/src/App.jsx
@@ -1,61 +1,31 @@
 import { useState } from 'react'
 
-const Header = (props) => {
-  return (
-    <>
-        <h1>{props.course}</h1>
-    </>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <>
-      <p>
-      {props.parts[0].name} {props.parts[0].exercises}
-      </p>
-      <p>
-      {props.parts[1].name} {props.parts[1].exercises}
-      </p>
-      <p>
-      {props.parts[2].name} {props.parts[2].exercises}
-      </p>
-    </>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <>
-      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-    </>
-  )
-}
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const [good, SetGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
   return (
+    <>
     <div>
-      <Header course = {course.name}/>
-      <Part parts = {course.parts} />
-      <Total parts = {course.parts} />
+      <h1>Give Feedback</h1>
     </div>
+    <div>
+      <button onClick={() => SetGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
+    </div>
+    <div>
+      <h2>Statistics:</h2>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+      <p> All {good + bad + neutral}</p>
+      <p>Average { (good - bad) / (good + neutral + bad)}</p>
+      <p>Positive = {good / (good + neutral + bad) * 100} %</p>
+    </div>
+    </>
   )
 }
 
