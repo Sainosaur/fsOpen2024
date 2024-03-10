5f310d110691bf47de79844f7953b7aaf9125ce4
adirajus <saiadi4002@gmail.com>
2024-03-10 14:46:11 +0000

    Exercise 5.7 made changes to support viewing/hiding details of blogs and placeholder like button

--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,29 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import { useState } from "react"
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    color: 'blue',
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const [disp, setDisp] = useState([{display: 'none'}, "View"])
+  const toggleVisibility = () => {
+    if (disp[1] == "View") {
+      setDisp([{}, "Hide"])
+    } else if (disp[1] == "Hide") {
+      setDisp([{display: 'none'},"View"])
+    }
+  }
+  return (
+  <div style={blogStyle}>
+    <p>{blog.title} {blog.author} <button onClick={() => toggleVisibility()}>{disp[1]}</button></p>
+    <div style={disp[0]}>
+      <p>{blog.url}</p>
+      <p>likes: {blog.likes}<button>like</button></p>
+    </div>
   </div>  
-)
+)}
 
 export default Blog
