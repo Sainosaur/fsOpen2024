1dcce838e0aa49712fa9612b00c2ea83d34e186a
adirajus <saiadi4002@gmail.com>
2024-03-18 18:05:52 +0000

    Got rid of parallel testing to ensure server is not overloaded

--- BloglistTesting/tests/blog.test.js
+++ BloglistTesting/tests/blog.test.js
@@ -33,7 +33,9 @@ describe('Login functionality', () => {
         // Checks to ensure error is present and the user has not been logged in to the system
         expect(page.getByText('wrong username or password')).toBeVisible()
         expect(page.getByText('Test-dude logged in')).not.toBeVisible()
-    }),
+    })
+})
+describe('Blog app functionality', () => {
     test('New blogs can be created', async ({page}) => {
         helper.testLogin(page)
         await page.getByText('new blog').click()
@@ -44,5 +46,21 @@ describe('Login functionality', () => {
         await page.getByText('a new blog Testing blogs... by Test-dude added!').waitFor()
         // Checks to ensure notfication stating blog has been created is visible. 
         expect(page.getByText('a new blog Testing blogs... by Test-dude added!')).toBeVisible()
+    }),
+    test('blogs can be liked', async ({page}) => {
+        helper.testLogin(page)
+        await page.getByText('new blog').click()
+        await page.locator('.Title').fill('Testing blogs...')
+        await page.locator('.Author').fill('Test-dude')
+        await page.locator('.Url').fill('http://www.example.com')
+        await page.getByRole('button', { name: 'create' }).click()
+        await page.getByText('a new blog Testing blogs... by Test-dude added!').waitFor()
+        // Reloads page such that the test blog added shows up
+        await page.reload()
+        await page.getByText('View').click()
+        await page.getByRole('button', { name: 'like'}).click()
+        await page.getByText('likes: 1').waitFor()
+        // After the process is completed the number of likes must be 1
+        expect(page.getByText('likes: 1')).toBeVisible()
     })
 })
