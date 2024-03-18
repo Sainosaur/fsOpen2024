bf966033892cd64386d6b0a013945b9ae1a066cc
adirajus <saiadi4002@gmail.com>
2024-03-18 18:50:25 +0000

    Exercise 5.21 implemented testing for deleting blogs

--- BloglistTesting/tests/blog.test.js
+++ BloglistTesting/tests/blog.test.js
@@ -1,5 +1,4 @@
 const {test, expect, beforeEach, describe} = require('@playwright/test')
-const helper = require('../helper')
 
 beforeEach(async ({page, request}) => {
     await request.post('http://localhost:3003/api/testing/reset')
@@ -11,6 +10,7 @@ beforeEach(async ({page, request}) => {
         }
     })
     await page.goto('http://localhost:5173')
+    await page.getByText('blogs').waitFor()
 })
 test('Login form is shown', ({page}) => {
     expect(page.getByText('username')).toBeDefined()
@@ -36,8 +36,13 @@ describe('Login functionality', () => {
     })
 })
 describe('Blog app functionality', () => {
+    beforeEach(async ({page}) => {
+        await page.locator('.UserField').fill('test')
+        await page.locator('.PwdField').fill('testing')
+        await page.getByRole('Button').click()
+        await page.getByText('Test-dude logged in').waitFor()
+    }),
     test('New blogs can be created', async ({page}) => {
-        helper.testLogin(page)
         await page.getByText('new blog').click()
         await page.locator('.Title').fill('Testing blogs...')
         await page.locator('.Author').fill('Test-dude')
@@ -48,7 +53,6 @@ describe('Blog app functionality', () => {
         expect(page.getByText('a new blog Testing blogs... by Test-dude added!')).toBeVisible()
     }),
     test('blogs can be liked', async ({page}) => {
-        helper.testLogin(page)
         await page.getByText('new blog').click()
         await page.locator('.Title').fill('Testing blogs...')
         await page.locator('.Author').fill('Test-dude')
@@ -60,10 +64,21 @@ describe('Blog app functionality', () => {
         await page.getByText('View').click()
         await page.getByRole('button', { name: 'like'}).click()
         await page.getByText('likes: 1').waitFor()
-        // After the process is completed the number of likes must be 1
+        // After the process is completed the number of likes must be
         expect(page.getByText('likes: 1')).toBeVisible()
     }),
-    test('blogs can be deleted by the user who created them', async ({page}) => {
-        testLogin(page)
+    test('blog can be deleted by user who created', async ({page}) => {
+        await page.getByText('new blog').click()
+        await page.locator('.Title').fill('Testing blogs...')
+        await page.locator('.Author').fill('Test-dude')
+        await page.locator('.Url').fill('http://www.example.com')
+        await page.getByRole('button', { name: 'create' }).click()
+        await page.getByText('a new blog Testing blogs... by Test-dude added!').waitFor()
+        // Reloads page such that the test blog added shows up
+        await page.reload()
+        await page.getByRole('Button', { name: 'view'}).click()
+        await page.getByRole('Button', { name: 'remove'}).click()
+        
+        expect(page.getByText('Testing blogs...')).not.toBeVisible()
     })
 })
