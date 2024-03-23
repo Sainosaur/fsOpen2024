2de65e2d6fa0b8c35f4710a39e9ead78e926d2fb
adirajus <saiadi4002@gmail.com>
2024-03-23 19:05:55 +0000

    Exercise 6.8 Created AnecdoteList component to handle all logic for the actual rendered list of anecdotes

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,28 +1,12 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { voteAction} from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/AnecdoteForm'
-const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
+import AnecdoteList from './components/AnecdoteList'
 
-  const vote = (id) => {
-     dispatch(voteAction(id))
-  }
+
+const App = () => {
 
   return (
     <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList />
       <AnecdoteForm />
     </div>
   )
