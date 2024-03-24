654cd02956d553048eee8a685c6adc797311f600
adirajus <saiadi4002@gmail.com>
2024-03-24 18:13:44 +0000

    Exercise 6.12 Implemented notification component andreducer

---
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit"
+const initialState = ''
+const notificationSlice = createSlice({
+    name: 'notification',
+    initialState,
+    reducers: {
+        setNotification(state, action) {
+            return action.payload
+        },
+        resetNotification(state, action) {
+            return initialState
+        }
+    }
+})
+
+export default notificationSlice.reducer
+
+export const { setNotification, resetNotification } = notificationSlice.actions
--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import anecReducer from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
+import notificationReducer from './reducers/notificationReducer'
 
 const store = configureStore({
     reducer: {
       anecdotes: anecReducer,
-      filter: filterReducer
+      filter: filterReducer,
+      notification: notificationReducer
     }
   })
 
