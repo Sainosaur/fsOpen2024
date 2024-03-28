c635fc7c49ea6b7acde648dd60385ec293654c0d
adirajus <saiadi4002@gmail.com>
2024-03-28 15:21:36 +0000

    Exercise 7.8 implmeneted useResource hook

---
+++ part7/ultimate-hooks/src/hooks/useResource.js
@@ -0,0 +1,15 @@
+import axios from "axios"
+
+const useResource = (url) => {
+    const create = async (data) => {
+
+    }
+    const resource = getAll()
+
+    return {
+        resource,
+        create
+    }
+}
+
+export default useResource
