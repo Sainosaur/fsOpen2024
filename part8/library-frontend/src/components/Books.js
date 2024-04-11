a44afd960e855ebea001f5910447d80d545f63de
adirajus <saiadi4002@gmail.com>
2024-04-11 18:08:44 +0100

    Implemented filtering functionality for library app

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,14 +1,25 @@
 import { useQuery } from '@apollo/client'
 import { GET_BOOKS } from '../queries'
+import { useState } from 'react'
 
 const Books = (props) => {
   const result = useQuery(GET_BOOKS)
+  const [filter, setFilter]  = useState('')
   if (result.loading) {
     return (
       <h1> Loading... </h1>
     )
   }
   const books = result.data.allBooks
+  const renderedBooks = books.filter(book => {
+    if (filter !== '') {
+      return book.genres.includes(filter)
+    } else {
+      return true
+    }
+
+    })
+  const allGenres = [].concat(...books.map(book => book.genres)).filter((genre, index, array) => array.indexOf(genre) === index)
   return (
     <div>
       <h2>books</h2>
@@ -20,7 +31,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {renderedBooks.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -29,6 +40,8 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+      {allGenres.map(genre => <button onClick={() => setFilter(genre)}>{genre}</button>)}
+      <button onClick={() => setFilter('')}>All Genres</button>
     </div>
   )
 }
